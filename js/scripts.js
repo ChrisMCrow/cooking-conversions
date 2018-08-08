dceda7d20d7727873922a694bdccd081e7efbced
Chris Crow and Chan Lee <chris@>
2018-08-08 13:57:20 -0700

    add styles, fix units to show up to -2 decimal points.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 // back-end
 
 var gramsToOunces = function(grams) {
-  return grams * 0.03527396;
+  return (grams * 0.03527396).toPrecision(2);
 };
 
 var ouncesToGrams = function(ounces) {
-  return ounces * 28.34952313;
+  return (ounces * 28.34952313).toFixed(2);
 };
 
 var gallonsToLiters = function(gallons) {
-  return gallons * 3.785411784;
+  return (gallons * 3.785411784).toFixed(2);
 };
 
 var litersToGallons = function(liters) {
-  return liters / 3.785411784;
+  return (liters / 3.785411784).toFixed(2);
 };
 
 // front-end
@@ -22,25 +22,25 @@ $(document).ready(function(){
   $("#one").submit(function(event){
     event.preventDefault();
     var gto = gramsToOunces($("input#gramstoounces").val());
-    $(".output").text(gto);
+    $(".output").text(gto + " ounces");
   });
 
   $("#two").submit(function(event){
     event.preventDefault();
     var otg = ouncesToGrams($("input#ouncestograms").val());
-    $(".output").text(otg);
+    $(".output").text(otg + " grams");
   });
 
   $("#three").submit(function(event){
     event.preventDefault();
     var gtl = gallonsToLiters($("input#gramstoounces").val());
-    $(".output").text(gtl);
+    $(".output").text(gtl + " liters");
   });
 
   $("#four").submit(function(event){
     event.preventDefault();
     var ltg = litersToGallons($("input#literstogallons").val());
-    $(".output").text(ltg);
+    $(".output").text(ltg + " gallons");
   });
 
 
